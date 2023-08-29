defb31a45be6b42145c260d501717e40ea0ab019
AntonioBukin <bav1905@gmail.com>
2023-08-29 14:53:15 +0300

    add hook

--- src/components/MyPhoneForm/MyPhoneForm.jsx
+++ src/components/MyPhoneForm/MyPhoneForm.jsx
@@ -1,78 +1,81 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import propTypes from 'prop-types';
 import styles from './my-phone-form.module.scss';
 
-class MyPhoneForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const MyPhoneForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    //value - значення яке ми записуємо в state
+    const { name, value } = e.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleChange = ({ target }) => {
-    const { name, value } = target; //value - значення яке ми записуємо в state
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset() {
-    this.setState({ name: '', number: '' });
-  }
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form onSubmit={this.handleSubmit} className={styles.form}>
-        <div className={styles.formGroup}>
-          <label>Name:</label>
-          <input
-            value={name}
-            name="name"
-            onChange={this.handleChange}
-            className={styles.textField}
-            placeholder="add name"
-            type="text"
-            //   name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          {/* <input className={styles.textField} placeholder="add name"/> */}
-        </div>
-        <div className={styles.formGroup}>
-          <label>Number:</label>
-          <input
-            value={number}
-            name="number"
-            onChange={this.handleChange}
-            className={styles.textField}
-            placeholder="add number"
-            type="text"
-            //   name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          {/* <input className={styles.textField} placeholder="add name"/> */}
-        </div>
-        <button className={styles.button} type="submit">
-          Add contacts
-        </button>
-      </form>
-    );
-  }
-}
-
-MyPhoneForm.propTypes = {
-  handleSubmit: propTypes.func.isRequired,
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <div className={styles.formGroup}>
+        <label>Name:</label>
+        <input
+          value={name}
+          name="name"
+          onChange={handleChange}
+          className={styles.textField}
+          placeholder="add name"
+          type="text"
+          //   name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        {/* <input className={styles.textField} placeholder="add name"/> */}
+      </div>
+      <div className={styles.formGroup}>
+        <label>Number:</label>
+        <input
+          value={number}
+          name="number"
+          onChange={handleChange}
+          className={styles.textField}
+          placeholder="add number"
+          type="text"
+          //   name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        {/* <input className={styles.textField} placeholder="add name"/> */}
+      </div>
+      <button className={styles.button} type="submit">
+        Add contacts
+      </button>
+    </form>
+  );
 };
 
 export default MyPhoneForm;
+
+MyPhoneForm.propTypes = {
+  onSubmit: propTypes.func.isRequired,
+};
+
